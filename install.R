e415a423eee3765ee2326c4eeb82de08633143f8
dbad <dbad@unc.edu>
2025-09-13 23:13:31 -0400

    Update install.R

--- install.R
+++ install.R
@@ -3,6 +3,5 @@ install.packages(c("readr","ggplot2","tidyr", "skimr"))
 install.packages("tidyverse", dependencies = TRUE)
 install.packages("rmarkdown", dependencies = TRUE)
 install.packages("IRkernel", dependencies = TRUE)
-install.packages("shiny", dependencies = TRUE)
 
 IRkernel::installspec(user = FALSE)
