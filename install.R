206679d283de46a0caf6fc0300fe0c20d38a4d54
dbad <dbad@unc.edu>
2025-09-13 21:27:49 -0400

    Update install.R

--- install.R
+++ install.R
@@ -1,5 +1,5 @@
 options(repos = c(CRAN = "https://cran.r-project.org"))
-install.packages(c("readr","ggplot2","tidyr"))
+install.packages(c("readr","ggplot2","tidyr", "skimr"))
 install.packages("tidyverse", dependencies = TRUE)
 install.packages("rmarkdown", dependencies = TRUE)
 install.packages("IRkernel", dependencies = TRUE)
